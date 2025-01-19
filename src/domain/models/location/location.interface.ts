a9589dfb2cbb4b3e8b1f84400d7d8b5e83b55d6a
Ilan Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-19 00:05:30 +0000

    feat: agregar documentación Swagger para modelos de asistentes, comentarios, eventos, ubicaciones, organizadores, oradores, patrocinadores y boletos, y actualizar rutas de API

--- src/domain/models/location/location.interface.ts
+++ src/domain/models/location/location.interface.ts
@@ -1,6 +1,39 @@
 import { Document } from 'mongoose';
 import { VenueType } from '../../enums';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ILocation:
+ *       type: object
+ *       required:
+ *         - name
+ *         - address
+ *         - capacity
+ *         - venueType
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: El nombre de la ubicación.
+ *         address:
+ *           type: string
+ *           description: La dirección de la ubicación.
+ *         capacity:
+ *           type: number
+ *           description: La capacidad de la ubicación.
+ *         venueType:
+ *           type: string
+ *           description: El tipo de lugar de la ubicación.
+ *           enum:
+ *             - indoor
+ *             - outdoor
+ *       example:
+ *         name: "Centro de Convenciones"
+ *         address: "123 Calle Principal"
+ *         capacity: 500
+ *         venueType: "indoor"
+ */
 export interface ILocation extends Document {
   name: string;
   address: string;
--- src/domain/models/organizer/organizer.interface.ts
+++ src/domain/models/organizer/organizer.interface.ts
@@ -1,5 +1,29 @@
 import { Document } from 'mongoose';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Organizer:
+ *       type: object
+ *       required:
+ *         - name
+ *         - description
+ *         - contact
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: El nombre del organizador.
+ *         description:
+ *           type: string
+ *           description: Una breve descripción del organizador.
+ *         contact:
+ *           type: string
+ *           description: Información de contacto del organizador.
+ *         website:
+ *           type: string
+ *           description: (Opcional) Sitio web del organizador.
+ */
 export interface IOrganizer extends Document {
   name: string;
   description: string;
